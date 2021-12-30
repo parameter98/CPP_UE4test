f6b5dda407dcd4db872a1e5ac9cbebc8c6a1489d
parameter98 <yeongukking@gmail.com>
2021-12-31 04:08:27 +0900

    Revert "cpp_gitignore"
    This reverts commit cf6daa8e42d6202efcd0c206fc54881ad06b783f.

--- CPP_test/Source/CPP_test/CPP_test.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CPP_test.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP_test, "CPP_test" );
