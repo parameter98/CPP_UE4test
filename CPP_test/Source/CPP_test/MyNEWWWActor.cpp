3206f2237c55a81b630f0a23224f253a420e567f
parameter98 <yeongukking@gmail.com>
2021-12-31 22:39:19 +0900

    [update] MyNEWWWActor 액터 추가

---
+++ CPP_test/Source/CPP_test/MyNEWWWActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyNEWWWActor.h"
+
+// Sets default values
+AMyNEWWWActor::AMyNEWWWActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyNEWWWActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyNEWWWActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
